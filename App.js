5e4b40dc300e59f0d4bac5f533446b9543a3d947
Sami El-sayed <samielsayed39@gmail.com>
2024-09-28 14:00:58 -0600

    Start page + signup

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import Onboarding from './pages/Onboarding.js';
-import Login from './pages/Login.js';
-import Launch from './pages/Launch.js';
-import PopulateInfo from './pages/PopulateInfo.js';
-import Start from './pages/Start.js';
-import Signup from './pages/Signup.js';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native'; // Import NavigationContainer
-import { createStackNavigator } from '@react-navigation/stack'; // Correct stack navigator import
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Onboarding from "./pages/Onboarding.js";
+import Login from "./pages/Login.js";
+import Launch from "./pages/Launch.js";
+import PopulateInfo from "./pages/PopulateInfo.js";
+import Start from "./pages/Start.js";
+import Signup from "./pages/Signup.js";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native"; // Import NavigationContainer
+import { createStackNavigator } from "@react-navigation/stack"; // Correct stack navigator import
 
-const Stack = createStackNavigator(); // Stack Navigation 
+const Stack = createStackNavigator(); // Stack Navigation
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Onboarding" component={Onboarding} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="PopulateInfo" component={PopulateInfo} />
@@ -31,9 +33,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
+    backgroundColor: "white",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
     paddingTop: 0,
   },
 });
--- pages/Start.js
+++ pages/Start.js
@@ -1,47 +1,64 @@
-import { View , Text , StyleSheet } from 'react-native' ; 
-import Button from "../components/Button.js" ; 
+import { View, Text, StyleSheet } from "react-native";
+import Button from "../components/Button.js";
 
-
-export default function Start({ navigation }){
-
-  function navigationHandle(){
-    navigation.navigate("Login")  
+export default function Start({ navigation }) {
+  function navigationHandle() {
+    navigation.navigate("Login");
   }
-  function navigationHandleTwo(){
-    navigation.navigate("Signup")  
+  function navigationHandleTwo() {
+    navigation.navigate("Signup");
   }
-  return(
+  return (
     <View style={styles.container}>
-      <Text>
-        Hello world this is Start
+      <View style={styles.placeholder} />
+      <Text style={styles.headerText}>
+        Empowering students and tutors with seamless scheduling.
       </Text>
+      <Text style={styles.paragraph}>
+        Connecting students and tutors has never been easier. Explore the
+        largest network of available sessions and schedule your learning or
+        teaching with ease.
+      </Text>
+
       <View style={styles.buttonContainer}>
-        <Button 
-          type="small" 
-          text="Login" 
-          callback={navigationHandle} 
-        />
-        <Button 
-          type="small" 
-          text="Sign up" 
-          callback={navigationHandleTwo} 
-        />
+        <Button type="small" text="Login" callback={navigationHandle} />
+        <Button type="small" text="Sign up" callback={navigationHandleTwo} />
       </View>
     </View>
-  )
-};
-
+  );
+}
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "column",
+    width: "100%",
+    gap: 10,
+    marginBottom: 16,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingTop: 60,
-    paddingBottom: 40,
+    backgroundColor: "white",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    marginBottom: 50,
   },
-  buttonContainer : {
-    flexDirection : 'column' , 
+  paragraph: {
+    fontSize: 15,
+    marginBottom: 30,
+    marginTop: 25,
+  },
+  headerText: {
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+
+  // Placeholder box, add cool graphic or something
+  placeholder: {
+    backgroundColor: "grey",
+    borderRadius: 32,
+    width: "100%",
+    height: 400,
+    marginBottom: 20,
   },
 });
