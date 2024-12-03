141f4b3e302814735ff216663bf50a7d9a43e705
unknown <ayubfmohammed@gmail.com>
2024-12-02 21:36:49 -0700

    dashboard touches

--- admin/src/pages/dashboard/index.jsx
+++ admin/src/pages/dashboard/index.jsx
@@ -249,7 +249,7 @@ export default function DashboardDefault() {
       <Grid item xs={12} md={7} lg={8}>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item>
-            <Typography variant="h5">Recent Orders</Typography>
+            <Typography variant="h5"></Typography>
           </Grid>
           <Grid item />
         </Grid>
@@ -280,127 +280,6 @@ export default function DashboardDefault() {
             </ListItemButton>
           </List>
           <ReportAreaChart />
-        </MainCard>
-      </Grid>
-
-      {/* row 4 */}
-      <Grid item xs={12} md={7} lg={8}>
-        <SaleReportCard />
-      </Grid>
-      <Grid item xs={12} md={5} lg={4}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Transaction History</Typography>
-          </Grid>
-          <Grid item />
-        </Grid>
-        <MainCard sx={{ mt: 2 }} content={false}>
-
-{/* ----------------------------------------------------------------------------------------------------------------------------- */}
-
-          <List
-            component="nav"
-            sx={{
-              px: 0,
-              py: 0,
-              '& .MuiListItemButton-root': {
-                py: 1.5,
-                '& .MuiAvatar-root': avatarSX,
-                '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-              }
-            }}
-          >
-
-{/* ----------------------------------------------------------------------------------------------------------------------------- */}
-
-            {/* <ListItemButton divider> */}
-            {/*   <ListItemAvatar> */}
-            {/*     <Avatar sx={{ color: 'success.main', bgcolor: 'success.lighter' }}> */}
-            {/*       <GiftOutlined /> */}
-            {/*     </Avatar> */}
-            {/*   </ListItemAvatar> */}
-            {/*   <ListItemText primary={<Typography variant="subtitle1">Order #002434</Typography>} secondary="Today, 2:00 AM" /> */}
-            {/*   <ListItemSecondaryAction> */}
-            {/*     <Stack alignItems="flex-end"> */}
-            {/*       <Typography variant="subtitle1" noWrap> */}
-            {/*         + $1,430 */}
-            {/*       </Typography> */}
-            {/*       <Typography variant="h6" color="secondary" noWrap> */}
-            {/*         78% */}
-            {/*       </Typography> */}
-            {/*     </Stack> */}
-            {/*   </ListItemSecondaryAction> */}
-            {/* </ListItemButton> */}
-
-{/* ----------------------------------------------------------------------------------------------------------------------------- */}
-
-            <ListItemButton divider>
-              <ListItemAvatar>
-                <Avatar sx={{ color: 'primary.main', bgcolor: 'primary.lighter' }}>
-                  <MessageOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={<Typography variant="subtitle1">Order #984947</Typography>} secondary="5 August, 1:45 PM" />
-              <ListItemSecondaryAction>
-                <Stack alignItems="flex-end">
-                  <Typography variant="subtitle1" noWrap>
-                    + $302
-                  </Typography>
-                  <Typography variant="h6" color="secondary" noWrap>
-                    8%
-                  </Typography>
-                </Stack>
-              </ListItemSecondaryAction>
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemAvatar>
-                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
-                  <SettingOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
-              <ListItemSecondaryAction>
-                <Stack alignItems="flex-end">
-                  <Typography variant="subtitle1" noWrap>
-                    + $682
-                  </Typography>
-                  <Typography variant="h6" color="secondary" noWrap>
-                    16%
-                  </Typography>
-                </Stack>
-              </ListItemSecondaryAction>
-            </ListItemButton>
-          </List>
-
-{/* ----------------------------------------------------------------------------------------------------------------------------- */}
-
-        </MainCard>
-        <MainCard sx={{ mt: 2 }}>
-          <Stack spacing={3}>
-            <Grid container justifyContent="space-between" alignItems="center">
-              <Grid item>
-                <Stack>
-                  <Typography variant="h5" noWrap>
-                    Help & Support Chat
-                  </Typography>
-                  <Typography variant="caption" color="secondary" noWrap>
-                    Typical replay within 5 min
-                  </Typography>
-                </Stack>
-              </Grid>
-              <Grid item>
-                <AvatarGroup sx={{ '& .MuiAvatar-root': { width: 32, height: 32 } }}>
-                  <Avatar alt="Remy Sharp" src={avatar1} />
-                  <Avatar alt="Travis Howard" src={avatar2} />
-                  <Avatar alt="Cindy Baker" src={avatar3} />
-                  <Avatar alt="Agnes Walker" src={avatar4} />
-                </AvatarGroup>
-              </Grid>
-            </Grid>
-            <Button size="small" variant="contained" sx={{ textTransform: 'capitalize' }}>
-              Need Help?
-            </Button>
-          </Stack>
         </MainCard>
       </Grid>
     </Grid>
